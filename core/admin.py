3a486911d32e6588576df81aee11a11e9af67e15
Stefan Petru <stefanpetru2018@gmail.com>
2026-06-09 16:09:01 +0300

    Implementat KDS, logica pentru mese si sistemul de gestiune a stocurilor

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Produs, Comanda, ElementComanda
+from .models import Produs, Comanda, ElementComanda, Masa, ProfilStaff, Ingredient, IngredientReteta
 
 admin.site.register(Produs)
 admin.site.register(Comanda)
 admin.site.register(ElementComanda)
+admin.site.register(Masa)
+admin.site.register(ProfilStaff)
+admin.site.register(Ingredient)
+admin.site.register(IngredientReteta)
